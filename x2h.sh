5f52b29fbf036dadb926e58612f9881cb2cb36eb
frogptux <101519323+frogptux@users.noreply.github.com>
2022-08-01 17:04:42 -0700

    Update x2h.sh

--- x2h.sh
+++ x2h.sh
@@ -3,6 +3,7 @@ for i in *.txt; do txt2html $i | tidy -q -asxml > ${i%.txt}.xml; done
 
 if [ ! -d "$DIR" ]; then # script statements if $DIR doesn't exist.
 mkdir $output 
-
+if [ -d "$DIR" ]; then
+  # Script statements if $DIR exists.
 mv *.txt $output
 fi
