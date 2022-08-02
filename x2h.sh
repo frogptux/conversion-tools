a26beaf6a6758ddc9a8b06026e45068783577879
frogptux <101519323+frogptux@users.noreply.github.com>
2022-08-01 17:06:13 -0700

    Update x2h.sh
    added comments and function to create directory if does or does not exist

--- x2h.sh
+++ x2h.sh
@@ -1,9 +1,9 @@
 #! /bin/sh
+
 for i in *.txt; do txt2html $i | tidy -q -asxml > ${i%.txt}.xml; done
 
 if [ ! -d "$DIR" ]; then # script statements if $DIR doesn't exist.
 mkdir $output 
-if [ -d "$DIR" ]; then
-  # Script statements if $DIR exists.
+if [ -d "$DIR" ]; then # Script statements if $DIR exists.
 mv *.txt $output
 fi
